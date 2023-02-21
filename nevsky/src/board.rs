c1d35220703b7226bb7f260d73319fdf855b7934
Jason <jason.johnson.081@gmail.com>
2023-02-21 21:47:14 +0000

    clean up enums

--- nevsky/src/board.rs
+++ nevsky/src/board.rs
@@ -5,15 +5,39 @@ pub enum PoliticalStatus {
 }
 
 #[derive(Debug)]
-pub struct LocaleInfo {
-    pub name: String,
-    pub conquerable: bool,
-    pub value: i32,
-    pub political_status: PoliticalStatus,
+pub enum Way {
+    Waterway,
+    Trackway,
 }
 
 #[derive(Debug)]
-pub enum Ways {
-    Waterway,
-    Trackway,
+pub enum Locale {
+    Castle,
+    Fort,
+    TradeRoute,
+    Bishopric,
+    City,
+    Novgorod,
+    Town,
+    Region,
+}
+
+#[derive(Debug)]
+pub enum Territory {
+    Livonia,
+    Estonia,
+    Rus,
+}
+
+#[macro_export]
+macro_rules! board {
+    ( $( $x:expr ),* ) => {
+        {
+            let mut temp_graph = Graph::new_undirected();
+            $(
+                temp_graph.add_node($x);
+            )*
+            temp_graph
+        }
+    };
 }
