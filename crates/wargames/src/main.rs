4d6233172e417cf49cbd291db1f391ea6687b1c7
jason <jason@johnson-net.ch>
2024-12-16 08:55:14 +0100

    we don't actually need a header component

--- crates/wargames/src/main.rs
+++ crates/wargames/src/main.rs
@@ -10,38 +10,31 @@ fn main() {
 
 #[component]
 fn App() -> impl IntoView {
-    let (count, set_count) = signal(0);
-
     view! {
         <Router>
-          <Header />
-            <main>
-          // all our routes will appear inside <main>
-              <Routes fallback=|| "Not found.">
-                <Route path=path!("/") view=Home/>
-                <Route path=path!("/games") view=Games/>
-              </Routes>
-            </main>
-      </Router>
-    }
-}
-
-#[component]
-fn Header() -> impl IntoView {
-    view! {
-        <header class="bg-teal-800 px-4 text-zinc-100">
+          <header class="bg-teal-800 px-4 text-zinc-100">
             <nav class="flex justify-between mb-6">
               <a href="/"><span class="text-xl font-semibold">Wargames</span></a>
               <div class="flex items-center space-x-4">
                 <a href="/games">Games</a>
               </div>
             </nav>
-        </header>
+          </header>
+          <main>
+            // all our routes will appear inside <main>
+            <Routes fallback=|| "Not found.">
+              <Route path=path!("/") view=Home/>
+              <Route path=path!("/games") view=Games/>
+            </Routes>
+          </main>
+      </Router>
     }
 }
 
 #[component]
 fn Home() -> impl IntoView {
+//    let (count, set_count) = signal(0);
+
     view! {
         <div class="container mx-auto">
             <h1 class="text-4xl font-semibold text-center mt-8">Welcome to Wargames</h1>
